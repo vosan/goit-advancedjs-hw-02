f870c7465e9eb9973e55da2cde4cca95e6037e54
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2026-06-10 22:10:15 -0700

    Implement promise generator

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  })
+    .then(value => {
+      iziToast.success({
+        title: 'OK',
+        message: `Fulfilled promise in ${value}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(value => {
+      iziToast.error({
+        title: 'Error',
+        message: `Rejected promise in ${value}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+});
